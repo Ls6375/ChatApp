a91c5f87a089e5c764ed12d24db52e224820d06d
l.s.dhillon6375 <l.s.dhillon6375@gmail.com>
2025-01-29 23:58:36 -0500

    Database connection and Basic Project folder structure made

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import authRoutes from './routes/auth.route.js';
+import dotenv from 'dotenv'
+import {connectDB} from './lib/db.js'
+
+const PORT = process.env.PORT || 5000;
+console.log(PORT);
+const app = express();
+
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, ()=>{
+	console.log(`Server is running on port ${PORT}`);
+	connectDB();
+})
+
